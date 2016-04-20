72a631d2d80211961eb5ed66cfd2364fcf03d828
Sejin Kim <skim@amplify.com>
2016-04-20 12:24:59 -0400

    add initial tests for pipeline

---
+++ test/unit/test_pipeline.py
@@ -0,0 +1,41 @@
+"""
+Tests of pipeline
+"""
+from unittest import TestCase
+
+
+from disco_aws_automation.pipeline import Pipeline
+
+
+class PipelineTests(TestCase):
+    '''Test Pipeline class'''
+
+    def test_size_as_rec_map_with_none(self):
+        """_size_as_recurrence_map works with None"""
+        pipeline = Pipeline({"some_key": None})
+        self.assertEqual(pipeline._val_as_recurrence_map("some_key"), {"": None})
+
+        pipeline = Pipeline({"some_key": ""})
+        self.assertEqual(pipeline._val_as_recurrence_map("some_key"), {"": None})
+
+    def test_size_as_rec_map_with_int(self):
+        """_size_as_recurrence_map works with simple integer"""
+        pipeline = Pipeline({"some_key": 5})
+        self.assertEqual(pipeline._val_as_recurrence_map("some_key", sentinel="0 0 * * *"),
+                         {"0 0 * * *": 5})
+
+    def test_size_as_rec_map_with_map(self):
+        """_size_as_recurrence_map works with a map"""
+        map_as_string = "2@1 0 * * *:3@6 0 * * *"
+        map_as_dict = {"1 0 * * *": 2, "6 0 * * *": 3}
+
+        pipeline = Pipeline({"some_key": map_as_string})
+        self.assertEqual(pipeline._val_as_recurrence_map("some_key"), map_as_dict)
+
+    def test_size_as_rec_map_with_duped_map(self):
+        """_size_as_recurrence_map works with a duped map"""
+        map_as_string = "2@1 0 * * *:3@6 0 * * *:3@6 0 * * *"
+        map_as_dict = {"1 0 * * *": 2, "6 0 * * *": 3}
+
+        pipeline = Pipeline({"some_key": map_as_string})
+        self.assertEqual(pipeline._val_as_recurrence_map("some_key"), map_as_dict)
